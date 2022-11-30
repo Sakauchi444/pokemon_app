7a2d7fb72ddccad669d43cfcbee8228e77cce441
Sakauchi444 <b1905429@planet.kanazawa-it.ac.jp>
2022-11-30 17:10:45 +0900

    finish step3

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,58 @@
 import 'package:flutter/material.dart';
+import 'poke_detail.dart';
+import 'poke_list_item.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokemon Flutter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(),
+      home: const TopPage(),
+    );
+  }
+}
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: ListView(
+      //   children: List.generate(10000, (id) => id)
+      //       .map((val) => PikaButton(index: val))
+      //       .toList(),
+      // ),
+    // );
+      body: ListView.builder(
+          itemCount: 10000,
+          // itemBuilder: (context, index) => PikaButton(index: index)),
+          itemBuilder: (context, index) => PokeListItem(index: index)),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class PikaButton extends StatelessWidget {
+  const PikaButton({Key? key, index}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: Image.network(
-                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-                  height: 100,
-                  width: 100,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  'No.25',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
+    return ElevatedButton(
+      child: const Text('pikachu'),
+      onPressed: () => {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => const PokeDetail(),
+            // builder: (BuildContext context) => const PokeListItem(index: index),
           ),
-          const Text(
-            'pikachu',
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-          ),
-          // const Chip(
-          //   label: Text('electric'),
-          //   backgroundColor: Colors.yellow,
-          // ),
-          Container(
-            child: const Text('electric'),
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.yellow,
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-        ]),
-      ),
+        ),
+      },
     );
   }
 }
